ec090e70c56b817074b9f29991f398b969de63c4
Masataka Sato <mas.0313.liberty@gmail.com>
2022-12-08 17:27:49 +0900

    close scanner

--- ITP1_1_B.java
+++ ITP1_1_B.java
@@ -4,6 +4,7 @@ class ITP1_1_B {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
+        sc.close();
         System.out.println(x * x * x);
     }
 }
