d8e5dc6c2acdb42fcdf480dac8d2ee619c93d3d1
Masataka Sato <mas.0313.liberty@gmail.com>
2022-12-11 11:51:10 +0900

    change source

--- ITP1_3_B.java
+++ ITP1_3_B.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 
 class ITP1_3_B {
     public static void main(String[] args) {
-        ArrayList<Integer> test = new ArrayList<Integer>();
+        ArrayList<Integer> inputList = new ArrayList<Integer>();
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
             Integer input = Integer.parseInt(scanner.nextLine());            
             if (input == 0) {
                 break;
             }
-            test.add(input);
+            inputList.add(input);
         }
         scanner.close();  
 
-        for(Integer i = 1; i-1 < test.size(); i++) {
-            String output = test.get(i-1).toString();
+        for(Integer i = 1; i-1 < inputList.size(); i++) {
+            String output = inputList.get(i-1).toString();
             System.out.println("Case " + i + ": " + output);
         }
     }
